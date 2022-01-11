e9c743671c57b965d383ada0ea58090ef89e47c2
Clive Lai <93177532+clivelai@users.noreply.github.com>
2022-01-11 11:04:18 +0800

    first commit
    this is the publish on 11th of January.
    the changes until wav recorder,
    KNN is still in progress.

---
+++ app/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+import os
+
+class AccountManager(BaseUserManager):
+    
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("User must have a email address.")
+        if not username:
+            raise ValueError("User must have a username.")
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+  
+class Audio(models.Model):
+    filename = models.CharField(max_length=255, blank=True)
+    file = models.FileField(upload_to='media/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+def get_profile_image_filepath(self, filename):
+    return f'profile_images/{self.pk}/{"profile_image.png"}'
+
+def get_default_profile_image():
+    return "profile_images/default_profile_images.png"
+
+def get_coughing_audio_filepath(self, filename):
+    return f'coughing_audio/{self.pk}/{"coughing_audio.wav"}'
+
+def get_default_coughing_audio():
+    return "coughing_audio/default_coughing_audio.wav"
+
+class Account(AbstractBaseUser):
+    
+    email           = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username        = models.CharField(max_length=30, unique=True)
+    # name            = models.CharField(max_length=64)
+    # phone           = models.CharField(max_length=12, unique=True)
+    date_joined     = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
+    last_login      = models.DateTimeField(verbose_name="last login", auto_now=True)
+    is_admin        = models.BooleanField(default=False)
+    is_active       = models.BooleanField(default=True)
+    is_staff        = models.BooleanField(default=False)
+    is_superuser    = models.BooleanField(default=False)
+    profile_image   = models.ImageField(max_length=255, upload_to=get_profile_image_filepath, null=True, blank=True, default=get_default_profile_image)
+    coughing_audio  = models.FileField(upload_to=get_coughing_audio_filepath, null=True, blank=True, default=get_default_coughing_audio)
+    hide_email      = models.BooleanField(default=True)
+    
+    objects = AccountManager()
+    
+    USERNAME_FIELD  = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.username
+    
+    def get_profile_image_filename(self):
+        return str(self.profile_image)[str(self.profile_image).index(f'profile_images/{self.pk}/')]
+    
+    def get_coughing_audio_filename(self):
+        return str(self.coughing_audio)[str(self.coughing_audio).index(f'coughing_audio/{self.pk}/')]
+             
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
---
+++ app/views.py
@@ -0,0 +1,166 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage
+from django.core.files.storage import FileSystemStorage
+from django.core.files import File
+
+from app.forms import RegistrationForm, AccountAuthenticationForm
+from app.models import Account
+from app.forms import AudioForm
+
+def welcome_view(request):
+    context = {}
+    return render(request, 'welcome.html', context)
+
+def auth_view(request, *args, **kwargs):
+    user = request.user
+    if user.is_authenticated: 
+        return redirect("dashboard")
+    destination = get_redirect_if_exists(request)
+    print("destination: " + str(destination))
+    
+    context = {}
+    
+    # login
+    if 'login-btn' in request.POST:
+        if request.POST:
+            form = AccountAuthenticationForm(request.POST)
+            if form.is_valid():
+                email = request.POST['email']
+                password = request.POST['password']
+                user = authenticate(email=email, password=password)
+                if user:
+                    login(request, user)
+                    if destination:
+                        return redirect(destination)
+                    return redirect("dashboard")
+
+        else:
+            form = AccountAuthenticationForm()
+
+        context['login_form'] = form
+    
+    # register
+    elif 'register-btn' in request.POST:
+        if request.POST:
+            form = RegistrationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                # email = form.cleaned_data.get('email').lower()
+                # raw_password = form.cleaned_data.get('password1')
+                # account = authenticate(email=email, password=raw_password)
+                # login(request, account)
+                destination = kwargs.get("next")
+                if destination:
+                    return redirect(destination)
+                return redirect('welcome')
+            else:
+                context['registration_form'] = form
+
+        else:
+            form = RegistrationForm()
+            context['registration_form'] = form
+        
+    return render(request, "auth.html", context)
+
+def get_redirect_if_exists(request):
+	redirect = None
+	if request.GET:
+		if request.GET.get("next"):
+			redirect = str(request.GET.get("next"))
+	return redirect
+
+# def menu_view(request, *args, **kwargs):
+#     context = {}
+    # user_id = kwargs.get("user_id")
+    # try:
+    #     account = Account.objects.get(pk=user_id)
+    # except:
+    #     return HttpResponse("Something went wrong.")
+    # if account:
+    #     context['id'] = account.id
+    #     context['username'] = account.username
+    #     context['email'] = account.email
+    #     context['profile_image'] = account.profile_image.url
+    #     context['hide_email'] = account.hide_email
+
+    #     # Define template variables
+    #     is_self = True
+    #     user = request.user
+    #     if user.is_authenticated and user != account:
+    #     	is_self = False
+    #     elif not user.is_authenticated:
+    #     	is_self = False
+
+    #     # Set the template variables to the values
+    #     context['is_self'] = is_self
+    #     context['BASE_URL'] = settings.BASE_URL
+    # return render(request, "accounts/menu.html", context)
+
+def dashboard_view(request):
+    context = {}
+    return render(request, 'accounts/dashboard.html', context)
+
+@csrf_exempt
+def test(request):
+    context = {}
+    return render(request, 'accounts/test.html', context)    
+
+@csrf_exempt
+def diagnose_view(request):
+    # request should be ajax and method should be POST.
+    # if request.method == 'POST' and request.is_ajax(): 
+    # is_ajax is deprecated and removed since django 4.0
+    if request.method == 'POST': 
+        
+        # delete old audio
+        request.user.coughing_audio.delete()
+        
+        
+        # recieve audio blob from ajax
+        file = request.FILES.get('audio')
+        print(file)
+                
+        # save audio to user model
+        request.user.coughing_audio.save("coughing_audio.wav", file)
+        request.user.save()
+        
+        
+        # get the form data
+        # audioForm = AudioForm(request.POST)
+        # reqAudio = request.POST.get('formData')
+        # # save the data and after fetch the object in instance
+        # if audioForm.is_valid():
+        #     audioForm.save()
+        # # else:
+        #     audioForm = AudioForm()
+        
+    context = {}
+    return render(request, 'accounts/diagnose.html', context)
+
+def diagnose_result_view(request):
+    context = {}
+    return render(request, 'accounts/diagnose-result.html', context)
+
+def profile_view(request):
+    context = {}
+    return render(request, 'accounts/profile.html', context)
+
+def change_profile_view(request):
+    context = {}
+    return render(request, 'accounts/changeProfile.html', context)
+
+def clinic_nearby_view(request):
+    context = {}
+    return render(request, 'accounts/clinicNearby.html', context)
+
+def history_view(request):
+    context = {}
+    return render(request, 'accounts/history.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect("welcome")
